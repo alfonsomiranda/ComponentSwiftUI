1fe9cfff570e8c381c784e52bb04ff4de42d4c21
Alfonso Miranda <alfonsomircas@gmail.com>
2021-10-02 19:32:47 +0200

    Modified text

--- Sources/Component/AMCButton.swift
+++ Sources/Component/AMCButton.swift
@@ -15,7 +15,7 @@ public struct AMCButton: View {
     }
     
     public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Welcome to AMCButton")
     }
 }
 
