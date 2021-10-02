6031026c7c79f4a9e26ad9c9cea7c24ee1476fb1
Alfonso Miranda <alfonsomircas@gmail.com>
2021-10-02 19:28:31 +0200

    Added public init

--- Sources/Component/AMCButton.swift
+++ Sources/Component/AMCButton.swift
@@ -7,13 +7,19 @@
 
 import SwiftUI
 
+@available(macOS 10.15, *)
 @available(iOS 13.0, *)
 public struct AMCButton: View {
+    public init() {
+            
+    }
+    
     public var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+@available(macOS 10.15, *)
 @available(iOS 13.0, *)
 struct AMCButton_Previews: PreviewProvider {
     static var previews: some View {
